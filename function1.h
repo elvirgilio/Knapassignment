9d715bd4df6f54e865b0f6a95d95d49655ba51c7
YaseenShawkat <YaseenShawkat>
2021-02-05 10:46:24 +0200

    Edited header file

--- function1.h
+++ function1.h
@@ -1,6 +1,4 @@
-#include <iostream>
-using namespace std;
-// A utility function that returns maximum of two integers
-int max(int a, int b) { return (a > b) ? a : b; }
 
-
+int max(int a, int b);
+int knapSack(int W, int wt[], int val[], int n);
+	
