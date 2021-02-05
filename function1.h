9f982630c09432de6ba04f8caf1abe677416ceac
Abdelaaty <abdelaatyrehab@gmail.com>
2021-02-06 00:00:56 +0200

    Edited aand corrected a few errors

--- function1.h
+++ function1.h
@@ -1,4 +1,6 @@
+// A utility function that returns maximum of two integers
+#include <iostream>
 
-int max(int a, int b);
+  int max (int a, int b);
 int knapSack(int W, int wt[], int val[], int n);
 	
--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include "function1.h"
-#include "fucntion1.cpp"
+#include <bits/stdc++.h>
+#include "function1.cpp"
+using namespace std;
 // Driver code
 int main()
 {
-int val[] = { 60, 100, 120 };
-int wt[] = { 10, 20, 30 };
-int W = 50;
-int n = sizeof(val) / sizeof(val[0]);
-cout << knapSack(W, wt, val, n);
-return 0;
+	int val[3] = { 60, 100, 120 };
+	int  wt[3] = { 10, 20, 30 };
+	int W = 50;
+	int  n = sizeof(val) / sizeof(val[0]);
+	cout << knapSack(W, wt, val, n);
+	return 0;
 }
-// This code is contributed by rathbhupendra
+
